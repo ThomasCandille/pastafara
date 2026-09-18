19cd6970a645a34fd185601392625298338b032c
Thomas Candille <thomascandille0508@gmail.com>
2026-09-18 15:38:19 +0200

    feat: Enhance user service and home screen with user retrieval and display

--- lib/src/providers/user_provider.dart
+++ lib/src/providers/user_provider.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import '../models/user_model.dart';
 import '../services/user_service.dart';
 
 final userServiceProvider = Provider<UserService>((ref) {
   return UserService();
+});
+
+final userProvider = FutureProvider.family<User?, String>((ref, userId) async {
+  final userService = ref.watch(userServiceProvider);
+  return userService.getUserFromDatabase(userId);
 });
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,10 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeView extends StatelessWidget {
+import '../providers/user_provider.dart';
+
+class HomeView extends ConsumerWidget {
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Text("Home View"));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+
+    if (userId == null) {
+      return const Scaffold(
+        body: Center(child: Text('Utilisateur non connecté')),
+      );
+    }
+
+    final userInfo = ref.watch(userProvider(userId));
+
+    return Scaffold(
+      body: userInfo.when(
+        data: (user) {
+          if (user == null) {
+            return const Center(child: Text('Utilisateur non trouvé'));
+          }
+
+          return Center(child: Text('Bienvenue, ${user.email}'));
+        },
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, _) => Center(child: Text('Erreur : $error')),
+      ),
+    );
   }
 }
