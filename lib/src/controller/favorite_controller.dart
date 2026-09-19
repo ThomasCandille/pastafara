d2973d7903aa8187ffb7e1d8aef51d3ba3ca684a
Thomas Candille <thomascandille0508@gmail.com>
2026-09-19 18:03:23 +0200

    feat: Implement favorite meals functionality with user provider and service integration

---
+++ lib/src/controller/favorite_controller.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final listFavProvider =
+    StateNotifierProvider<FavoriteController, List<Map<String, dynamic>>>(
+        (ref) {
+  return FavoriteController();
+});
+
+class FavoriteController extends StateNotifier<List<Map<String, dynamic>>> {
+  FavoriteController() : super([]);
+
+  void addToFav(int index, List<Map<String, dynamic>> data) {
+    data[index]['isFavorite'] = true;
+    final result =
+        data.where((element) => element['isFavorite'] == true).toList();
+    state = [...result];
+  }
+
+  void removeFromFav(int id) {
+    for (final item in state) {
+      if (item['id'] == id) {
+        item['isFavorite'] = false;
+      }
+    }
+    final result =
+        state.where((element) => element['isFavorite'] == true).toList();
+    state = [...result];
+  }
+}
+
+//https://medium.com/@purboyndra/flutter-tutorial-add-to-favorite-and-remove-from-favorite-with-riverpod-1cd419bc0b2a
